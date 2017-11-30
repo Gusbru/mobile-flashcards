69362e208f6473f443eebd1d80edc17a84c089e3
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-11-29 22:29:28 -0200

    func: decks and newDecks

--- App.js
+++ App.js
@@ -1,17 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View, SectionList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, StatusBar } from 'react-native';
 import Decks from './components/Decks';
+import NewDecks from './components/NewDecks';
+import { Constants } from 'expo';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { black, white, purple } from './utils/colors';
+
+
+function MainStatusBar({ backgroundColor, ...props}) {
+  return(
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  )
+}
+
+const Tabs = TabNavigator({
+  Decks: {
+    screen: Decks,
+    navigationOptions: {
+      tabBarLabel: 'Decks'
+    }
+  },
+  NewDecks: {
+    screen: NewDecks,
+    navigationOptions: {
+      tabBarLabel: 'New Decks'
+    }
+  }
+});
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+});
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <SectionList
-          renderItem
-        />
+      <View style={{flex:1}}>
+        <MainStatusBar backgroundColor={black} barStyle='light-content'/>
+        <Tabs />
+        
+
       </View>
     );
   }
--- components/Decks.js
+++ components/Decks.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Platform } from 'react-native';
+import { Text, View, StyleSheet, Platform, FlatList } from 'react-native';
+import TextButton from './TextButton';
 import { white } from '../utils/colors';
+
+// TODO: Read this from AsyncStorage
+const db = {
+  React: {
+    title: 'React',
+    questions: [
+      {
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces'
+      },
+      {
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event'
+      }
+    ]
+  },
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      }
+    ]
+  }
+};
 
 class Decks extends Component {
   render(){
+    data = Object.keys(db).map(item => ({key: item}));
     return(
-      <View style={styles.container}>
-        <Text>test</Text>
+      <View style={{flex:1}}>
+        <FlatList
+          data={data}
+          renderItem={({item}) => (
+            <TextButton style={styles.container}>
+              {item.key}
+            </TextButton>
+          )}
+        />
       </View>
     );
   }
@@ -20,7 +55,9 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     marginRight: 10,
     marginTop: 17,
+    marginBottom: 17,
     justifyContent: 'center',
+    alignItems: 'center',
     shadowRadius: 3,
     shadowOpacity: 0.8,
     shadowColor: 'rgba(0, 0, 0, 0.24)',
