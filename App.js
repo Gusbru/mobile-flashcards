27eefc11352a4dba2b43a035161918c91e06d8ac
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-11-29 22:56:23 -0200

    chore: add icons to tab

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, FlatList, StatusBar, Platform } from 'react-native';
 import Decks from './components/Decks';
 import NewDecks from './components/NewDecks';
 import { Constants } from 'expo';
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import { black, white, purple } from './utils/colors';
-
+import { Ionicons } from '@expo/vector-icons'
 
 function MainStatusBar({ backgroundColor, ...props}) {
   return(
@@ -19,21 +19,17 @@ const Tabs = TabNavigator({
   Decks: {
     screen: Decks,
     navigationOptions: {
-      tabBarLabel: 'Decks'
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <Ionicons name={Platform.OS === 'ios' ? 'ios-play' : 'md-play'} size={30} color={tintColor}/>
     }
   },
   NewDecks: {
     screen: NewDecks,
     navigationOptions: {
-      tabBarLabel: 'New Decks'
+      tabBarLabel: 'New Decks',
+      tabBarIcon: ({ tintColor }) => <Ionicons name={Platform.OS === 'ios' ? 'ios-add-circle-outline' : 'md-add-circle-outline'} size={30} color={tintColor}/>
     }
   }
-});
-
-const MainNavigator = StackNavigator({
-  Home: {
-    screen: Tabs,
-  },
 });
 
 export default class App extends React.Component {
@@ -42,8 +38,6 @@ export default class App extends React.Component {
       <View style={{flex:1}}>
         <MainStatusBar backgroundColor={black} barStyle='light-content'/>
         <Tabs />
-        
-
       </View>
     );
   }
