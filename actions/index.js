33ef1f9c32f3f6c600492bca9690089fed82e3ac
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-12-04 20:55:45 -0200

    func: use AsyncStorage to save decks and cards information

--- actions/index.js
+++ actions/index.js
@@ -1,37 +1,11 @@
+import { fetchAllDecks } from '../utils/api';
+
 export const RECEIVE_ALL_DECKS = 'RECEIVE_ALL_DECKS';
 export const RECEIVE_CARDS = 'RECEIVE_CARDS';
 
-// Get all decks from the server
-const allDecks = {
-  React: {
-    title: 'React',
-    questions: [
-      {
-        question: 'What is React?',
-        answer: 'A library for managing user interfaces'
-      },
-      {
-        question: 'Where do you make Ajax requests in React?',
-        answer: 'The componentDidMount lifecycle event'
-      }
-    ]
-  },
-  JavaScript: {
-    title: 'JavaScript',
-    questions: [
-      {
-        question: 'What is a closure?',
-        answer: 'The combination of a function and the lexical environment within which that function was declared.'
-      }
-    ]
-  }
-}
-
 export const getDecks = () => async(dispatch) => {
-  // no lugar de allDecks vai entrar o "get" do firebase
-  decks = allDecks
-  dispatch(addDecks(decks));
-  
+  fetchAllDecks()
+  .then(data => dispatch(addDecks(data)))
 }
 
 const addDecks = (decks) => (
@@ -41,7 +15,7 @@ const addDecks = (decks) => (
   }
 )
 
-const addDeck = (item) => {
+const addNewDeck = (item) => {
   
 }
 
--- utils/api.js
+++ utils/api.js
@@ -3,5 +3,40 @@ import { AsyncStorage } from 'react-native';
 const STORAGE_KEY = 'flashcards:data';
 
 export function fetchAllDecks() {
+  return AsyncStorage.getItem(STORAGE_KEY)
+  .then(initialData)
+}
+
+initialData = (data) => {
+  data === null
+  && AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(
+      {
+        React: {
+          title: 'React',
+          questions: [
+            {
+              question: 'What is React?',
+              answer: 'A library for managing user interfaces'
+            },
+            {
+              question: 'Where do you make Ajax requests in React?',
+              answer: 'The componentDidMount lifecycle event'
+            }
+          ]
+        },
+        JavaScript: {
+          title: 'JavaScript',
+          questions: [
+            {
+              question: 'What is a closure?',
+              answer: 'The combination of a function and the lexical environment within which that function was declared.'
+            }
+          ]
+        }
+      }
+    )
+  )
   
+  return AsyncStorage.getItem(STORAGE_KEY)
+  .then(out => JSON.parse(out))
 }
