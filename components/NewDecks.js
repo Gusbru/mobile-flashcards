6863b5e7ca0f563eb8d36f4ea6856fddd0072b06
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-12-05 10:55:54 -0200

    chore: style new deck screen

--- components/NewDecks.js
+++ components/NewDecks.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text, StyleSheet, TextInput, Alert, Keyboard } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Alert, Keyboard, Platform } from 'react-native';
 import TextButton from './TextButton';
 import { saveDeckTitle } from '../actions';
+import { lightPurp, white, black, red } from '../utils/colors';
 
 class NewDecks extends Component {
   state = {
@@ -33,34 +34,27 @@ class NewDecks extends Component {
 
   render(){
     return(
-      <View>
+      <View style={{flex:1, justifyContent: 'space-between'}}>
         <Text style={styles.titleText}>
           New Deck Title
         </Text>
         <TextInput
+          style={styles.deckTitleText}
           placeholder="Deck Title"
           value={this.state.title}
           onChangeText={(title) => this.setState({title})}
           onSubmitEditing={Keyboard.dismiss}
         />
         <TextButton
-        onPress={() => this.updateTitle()}>
+          style={[styles.container, styles.buttonText]} 
+          onPress={() => this.updateTitle()}
+        >
           Submit
         </TextButton>
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  titleText: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    paddingTop: 30,
-    paddingBottom: 20,
-  }
-})
 
 const mapStateToProps = (actions) => (
   {
@@ -73,5 +67,55 @@ const mapDispatchToProps = (dispatch) => (
     newDeckTitle: (title) => dispatch(saveDeckTitle(title)),
   }
 )
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: lightPurp,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    marginBottom: 17,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+  },
+  deckTitleText: {
+    fontSize: 20,
+    fontWeight: 'normal',
+    color: black,
+    paddingLeft: 40,
+    paddingRight: 40,
+    paddingTop: 20,
+    paddingBottom: 20,
+    margin: 20,
+    justifyContent: 'center',
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    borderWidth: 2,
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: white,
+    paddingTop: 20,
+    paddingBottom: 20,
+    padding: 20
+  },
+  titleText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingTop: 30,
+    paddingBottom: 20,
+  },
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(NewDecks);
